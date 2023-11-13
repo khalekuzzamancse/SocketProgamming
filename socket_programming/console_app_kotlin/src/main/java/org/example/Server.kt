343405a46be1dd6890be31fb3602b6e755ec86d5
Khalekuzzaman <74848657+khalekuzzamancse@users.noreply.github.com>
2023-11-14 03:09:07 +0600

    created

--- socket_programming/console_app_kotlin/src/main/java/org/example/Server.kt
+++ socket_programming/console_app_kotlin/src/main/java/org/example/Server.kt
@@ -5,6 +5,7 @@ import java.io.IOException
 import java.io.InputStream
 import java.net.ServerSocket
 import java.net.Socket
+import java.nio.charset.Charset
 
 
 class Server(
@@ -40,14 +41,39 @@ class Server(
         TODO("Not yet implemented")
     }
 
+
     override fun observeReceiveData() {
+        val headerFooterSLength = 10
+        var data = mutableListOf<Byte>()
         while (true) {
             try {
+                val rec = ByteArray(20)
                 val inputStream: InputStream = socket.getInputStream()
-                val byteValue = inputStream.read()
-                if (byteValue != -1) {
-                    val characterValue = byteValue.toChar()
-                    print("$characterValue")
+                val receivedLengthInBytes = inputStream.read(rec)
+                val isDataReceived = receivedLengthInBytes != -1
+                if (isDataReceived) {
+                    val isEithereHeaderOrFooteer = receivedLengthInBytes == headerFooterSLength
+                    if (isEithereHeaderOrFooteer) {
+                        val info = String(rec, 0, receivedLengthInBytes, Charsets.UTF_8)
+                        val isFooter = info.trimEnd() == "@END"
+                        val isHeader = !isFooter
+                        if (isHeader) {
+                            data = mutableListOf()
+                        } else if (isFooter) {
+                            // val res= String(arr, 0, data.size, Charset.defaultCharset())
+                            //  println(res)
+                            println(data)
+                            data = mutableListOf()
+
+                        }
+                    } else {//then it is data
+                        val dataLengthInBytes = 1
+                        val characterValue = String(rec, 0, dataLengthInBytes, Charsets.UTF_8)
+                        print("$characterValue")
+                        data.add(rec[0])
+
+                    }
+
                 }
             } catch (e: IOException) {
                 e.printStackTrace()
